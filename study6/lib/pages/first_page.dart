5cffb32f80385177f625234a960fb479d147cc2e
EatFan <eatfan0921@163.com>
2025-09-19 17:20:03 +0800

    study6 底部Tab导航栏

--- study6/lib/pages/first_page.dart
+++ study6/lib/pages/first_page.dart
@@ -19,7 +19,8 @@ class _FirstPageState extends State<FirstPage> {
   int _selectedIndex = 0;
 
   // 定义导航栏显示可以显示哪些页面
-  final List _pages = [
+  // 页面列表
+  final List<Widget> _pages = [
     // 主页页面
     HomePage(
       title: "主页",
@@ -28,6 +29,13 @@ class _FirstPageState extends State<FirstPage> {
     ProfilePage(),
     // 设置页面
     SettingsPage()
+  ];
+
+  // 对应的标题
+  final List<String> _titles = [
+    "主页页面",
+    "资料页面",
+    "设置页面",
   ];
 
   // 跳转导航栏中的页面
@@ -41,34 +49,53 @@ class _FirstPageState extends State<FirstPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("第一个页面"),
+        title: Text(
+          _titles[_selectedIndex]
+        ),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
       body: _pages[_selectedIndex],
 
       // 定义底部导航栏列表物品
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _navigateBottomBar,
-        items: [
-          // Home页面
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+      bottomNavigationBar: Container(
+        color: Colors.red,
+        height: 95,
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomBar,
+          selectedItemColor: Colors.blue,       // 选中的颜色
+          unselectedItemColor: Colors.grey,
+          items: [
+            // Home页面
+            BottomNavigationBarItem(
+              icon: Icon(
+                  Icons.home,
+                size: 20,
+              ),
               label: "主页",
-          ),
-          // 资料页面
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "资料"
-          ),
-          // 设置页面
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: "设置"
-          ),
-        ],
-      ),
+            ),
+            // 资料页面
+            BottomNavigationBarItem(
+                icon: Icon(
+                    Icons.person,
+                  size: 20,
+                ),
+                label: "资料"
+            ),
+            // 设置页面
+            BottomNavigationBarItem(
+                icon: Icon(
+                    Icons.settings,
+                  size: 20,
+                ),
+                label: "设置"
+            ),
+          ],
+        ),
+      )
+
+
     );
   }
 
